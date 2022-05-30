69045beb3316b7a9e4d312166bd8d07500452a07
Hez <mokuahesbon254@gmail.com>
2022-05-30 21:00:50 +0300

    Remove redundant stuff

--- js/script.js
+++ js/script.js
@@ -63,30 +63,15 @@ var switchMenuToActive = function () {
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
   
-// TODO: STEP 0: Look over the code from 
-// *** start *** 
-// to 
-// *** finish *** 
-// below.
-// We changed this code to retrieve all categories from the server instead of
-// simply requesting home HTML snippet. We now also have another function
-// called buildAndShowHomeHTML that will receive all the categories from the server
-// and process them: choose random category, retrieve home HTML snippet, insert that
-// random category into the home HTML snippet, and then insert that snippet into our
-// main page (index.html).
-//
-// TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML, 
-// so it can be called when server responds with the categories data.
 
-// *** start ***
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
-// *** finish **
+
 
 
 // Builds HTML for the home page based on categories array
